4c30e8620b302b1253e21f16a3993c3fdaddf4cb
Frankie Chan <frankie.c.287@gmail.com>
2017-11-22 13:38:17 -0500

    Updated Responsive about text

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	
-	$('a[href^="#"]').click(function () {
+    $('a[href^="#"]').click(function () {
     $('html, body').animate({
         scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
     }, 500);
